f4768d937bbfbb3d7fe1a3f2cbd9956b4cd6a42f
leah <siuleelam999@gmail.com>
2025-10-27 22:38:53 +0800

    fix bug

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,10 +16,14 @@ const Projects = () => {
           {projects.map(({ id, title, imageSrc, imageAlt, description, technologies, liveDemoLink, codeLink }) => (
             <div
               key={id}
-              className="max-w-5xl flex flex-col lg:grid lg:grid-cols-[400px_1fr] border-2 p-4 rounded-2xl border-gray-700 w-full text-white"
+              className="max-w-5xl flex flex-col lg:grid lg:grid-cols-[400px_1fr] lg:grid-rows-[250px] border-2 p-4 rounded-2xl border-gray-700 w-full text-white"
             >
               <div className="w-full border-2 border-gray-700 lg:h-full rounded-2xl py-3">
-                <img alt={imageAlt} src={imageSrc} className="w-full h-full object-contain object-center" />
+                <img
+                  alt={imageAlt}
+                  src={imageSrc}
+                  className="w-full object-contain object-center"
+                />
               </div>
 
               <div className="flex flex-col lg:h-full lg:justify-between py-6 lg:px-6 lg:py-3">
