459d845d9361f3b550ba9aecc3588acd99b0f0e7
leah <siuleelam999@gmail.com>
2025-10-27 21:18:00 +0800

    update assets src

---
+++ src/assets/projects/index.js
@@ -0,0 +1,5 @@
+import storeImg from "./store-management-app.png";
+import todoImg from "./todo-app.png";
+import weatherImg from "./weather-app.png";
+
+export { storeImg, todoImg, weatherImg }; 
---
+++ src/assets/tech-icons/index.js
@@ -0,0 +1,10 @@
+import cssImg from "./CSS3.svg";
+import expressImg from "./Express.svg";
+import htmlImg from "./HTML5.svg";
+import jsImg from "./JavaScript.svg";
+import mongoImg from "./MongoDB.svg";
+import nodeImg from "./Node.js.svg";
+import reactImg from "./React.svg";
+import tailwindImg from "./TailwindCSS.svg";
+
+export { cssImg, expressImg, htmlImg, jsImg, mongoImg, nodeImg, reactImg, tailwindImg };
